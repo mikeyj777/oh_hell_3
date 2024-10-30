2df785e05d65a9356c50a26d29dd061b65dc83be
Michael James <mikeyj777@gmail.com>
2024-10-29 23:18:54 -0400

    4-player layout fixed

--- client/src/game/CardGame.js
+++ client/src/game/CardGame.js
@@ -5,7 +5,7 @@ import CardPlacement from './CardPlacement';
 import './CardGameAndPlacement.css';
 
 const CardGame = () => {
-  const dealCards = new DealCards(5, 10);
+  const dealCards = new DealCards(4, 13);
 
   return (
     <div className="card-game-container">
